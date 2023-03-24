bc856c0996be2779fa0b69e2c1e642d4e6ccf95b
Seyarada <Seyarada@gmail.com>
2023-03-24 10:32:37 +0100

    🔧 CommandFlag will no longer execute empty commands, print debug the case the command is empty

--- Plugin/src/main/java/net/seyarada/pandeloot/flags/effects/CommandFlag.java
+++ Plugin/src/main/java/net/seyarada/pandeloot/flags/effects/CommandFlag.java
@@ -1,16 +1,30 @@
 package net.seyarada.pandeloot.flags.effects;
 
+import net.seyarada.pandeloot.Logger;
 import net.seyarada.pandeloot.drops.ItemDropMeta;
 import net.seyarada.pandeloot.flags.FlagEffect;
 import net.seyarada.pandeloot.flags.types.IGeneralEvent;
 import org.bukkit.Bukkit;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @FlagEffect(id="command", description="Executes a command")
 public class CommandFlag implements IGeneralEvent {
 
 	@Override
 	public void onCallGeneral(ItemDropMeta meta) {
-		Bukkit.dispatchCommand(Bukkit.getConsoleSender(), meta.getString());
+		if(meta.getString()!=null) {
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), meta.getString());
+		} else {
+			String sB =
+					"Tried to execute an empty command!" + System.lineSeparator() +
+					"Information: " + System.lineSeparator() +
+					"- FlagPack > " + meta.iDrop().getFlagPack() + System.lineSeparator() +
+					"- Player > " + meta.lootDrop().p.getDisplayName() + System.lineSeparator() +
+					"- Command > " + Collections.singletonList(meta.values());
+			Logger.userWarning(sB);
+		}
 	}
 
 }
