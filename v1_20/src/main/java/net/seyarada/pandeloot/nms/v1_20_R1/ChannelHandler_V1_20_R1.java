13db9628b58ca837e5e577bc25382c2e4c1fc4ad
Seyarada <Seyarada@gmail.com>
2023-06-16 21:56:06 +0200

    1.20 Support

---
+++ v1_20/src/main/java/net/seyarada/pandeloot/nms/v1_20_R1/ChannelHandler_V1_20_R1.java
@@ -0,0 +1,37 @@
+package net.seyarada.pandeloot.nms.v1_20_R1;
+
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelPromise;
+import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
+import net.minecraft.server.level.ServerPlayer;
+import net.seyarada.pandeloot.nms.NMSManager;
+
+public class ChannelHandler_V1_20_R1 extends ChannelDuplexHandler {
+
+    private final ServerPlayer player;
+
+    public ChannelHandler_V1_20_R1(ServerPlayer player) {
+        this.player = player;
+    }
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object packet, ChannelPromise promise) throws Exception {
+
+        // Item Visibility
+        if (packet instanceof ClientboundAddEntityPacket) {
+            int id = ((ClientboundAddEntityPacket) packet).getId();
+            if(NMSManager.isHiddenFor(id, player.getBukkitEntity().getUniqueId())) {
+                return;
+            }
+        }
+
+        super.write(ctx, packet, promise);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object packet) throws Exception {
+        super.channelRead(ctx, packet);
+    }
+
+}
